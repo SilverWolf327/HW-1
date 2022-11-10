0c7e78b93018929b0e4f0ca1c1eab959901f1f3c
Aleksandr Guzeev <aleksandr.guzeeff@yandex.ru>
2022-11-10 18:29:45 +0300

    Initial commit

---
+++ task.js
@@ -0,0 +1,28 @@
+function solveEquation(a, b, c) {
+  'use strict';
+
+  let arr = [];
+  let x1;
+  let x2;
+  let d = Math.pow(b, 2) - 4*a*c;
+  
+  if (d === 0) {
+      x1 =  -b/(2 * a);
+      arr.push(x1);
+  };
+    
+  if (d > 0) {
+      x1 = (-b + Math.sqrt(d))/(2 * a);
+      x2 = (-b - Math.sqrt(d))/(2 * a);
+      arr.push(x1, x2);
+  }
+  return arr;
+}
+
+function calculateTotalMortgage(percent, contribution, amount, date) {
+  let totalAmount;
+
+  // код для задачи №2 писать здесь
+
+  return totalAmount;
+}
